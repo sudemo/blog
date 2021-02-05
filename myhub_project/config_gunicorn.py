817452ba40f83c0af0924b690bec63b57d1a4320
zzw <1733112322@qq.com>
2021-02-05 11:05:31 +0800

    gunicorn reconfig/pipfile &lock

--- myhub_project/config_gunicorn.py
+++ myhub_project/config_gunicorn.py
@@ -31,10 +31,12 @@ loglevel = 'info'
 reload = True
 debug = True
 
+capture_output = False
+
 bind = "%s:%s" % ("0.0.0.0", 8000)
 # pidfile = '%s/run/%s.pid' % (path_of_current_dir, _file_name)
-# errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
-# accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
+errorlog = '%s/logs/error.log' % (path_of_current_dir)
+accesslog = '%s/logs/access.log' % (path_of_current_dir)
 """
 其每个选项的含义如下：
 h          remote address
